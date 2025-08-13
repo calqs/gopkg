bc3b58d35fe9bc870ebf139d22e7c1af083cc2c8
monkeydioude <duris.julien@gmail.com>
2025-08-13 23:51:51 +0200

    reorder request. Need more docs and tests

--- router/request/request.go
+++ router/request/request.go
@@ -1,9 +1,38 @@
 package request
 
-import "net/http"
+import (
+	"net/http"
+	"net/url"
+	"reflect"
+	"strconv"
+	"strings"
+)
 
-type Request[DataT any] interface {
-	GetData(*http.Request) (*DataT, error)
+func bindValues[T any](dst *T, vals url.Values) error {
+	v := reflect.ValueOf(dst).Elem()
+	t := v.Type()
+
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		tag := field.Tag.Get("query")
+		if tag == "" {
+			tag = strings.ToLower(field.Name)
+		}
+
+		if val := vals.Get(tag); val != "" {
+			fv := v.Field(i)
+			switch fv.Kind() {
+			case reflect.String:
+				fv.SetString(val)
+			case reflect.Int:
+				if n, err := strconv.Atoi(val); err == nil {
+					fv.SetInt(int64(n))
+				}
+				// @TODO: add more types
+			}
+		}
+	}
+	return nil
 }
 
 func ExtractData[DataT any](r *http.Request) (*DataT, error) {
