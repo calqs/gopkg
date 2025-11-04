927db3b75826d56fb2691935891eda1107add207
monkeydioude <duris.julien@gmail.com>
2025-11-04 23:48:01 +0100

    fix: use HandlerFactory interface in routing, to allow other implementations

--- router/handler/handler.go
+++ router/handler/handler.go
@@ -21,11 +21,6 @@ type Request[ParamsT any] struct {
 	Params  *ParamsT
 }
 
-// GenHandler is mostly used to wrap a Handler in a generic way.
-// This way, we can have handlers having a concrete struct as return type
-// instead of an interface
-type GenHandler[RequestT any, ResponseT response.Response] func(*Request[RequestT]) ResponseT
-
 type Method string
 
 func (m Method) String() string {
@@ -37,15 +32,22 @@ type MethodHandler struct {
 	Handler Handler
 }
 
-func GenHandlerToHandler[RequestT any, ResponseT response.Response](
-	handler GenHandler[RequestT, ResponseT],
-) func(r *http.Request) response.Response {
+type HandlerTransformer[RequestT any] interface {
+	Transform() Handler
+}
+
+// GenHandler is mostly used to wrap a Handler in a generic way.
+// This way, we can have handlers having a concrete struct as return type
+// instead of an interface
+type GenHandler[RequestT any, ResponseT response.Response] func(*Request[RequestT]) ResponseT
+
+func (genHandler GenHandler[RequestT, ResponseT]) Transform() Handler {
 	return func(r *http.Request) response.Response {
 		res, err := request.ExtractData[RequestT](r)
 		if err != nil {
 			return response.InternalServerError("invalid data type", err)
 		}
-		return handler(&Request[RequestT]{
+		return genHandler(&Request[RequestT]{
 			Request: r,
 			Params:  res,
 		})
--- router/public/public_method.go
+++ router/public/public_method.go
@@ -7,37 +7,40 @@ import (
 	"github.com/calqs/gopkg/router/response"
 )
 
-func routeIt[RequestT any, ResponseT response.Response](method string, h handler.GenHandler[RequestT, ResponseT]) http.Handler {
+func RouteIt[RequestT any, ResponseT response.Response](
+	method string,
+	h handler.HandlerTransformer[RequestT],
+) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != method {
 			return
 		}
-		res := handler.GenHandlerToHandler(h)(r)
+		res := h.Transform()(r)
 		res.Send(w)
 	})
 }
 
 // Get is a wrapper around a generic handler, forcing the GET HTTP verb
 func Get[RequestT any, ResponseT response.Response](h handler.GenHandler[RequestT, ResponseT]) http.Handler {
-	return routeIt(http.MethodGet, h)
+	return RouteIt[RequestT, ResponseT](http.MethodGet, h)
 }
 
 // Post is a wrapper around a generic handler, forcing the POST HTTP verb
 func Post[RequestT any, ResponseT response.Response](h handler.GenHandler[RequestT, ResponseT]) http.Handler {
-	return routeIt(http.MethodPost, h)
+	return RouteIt[RequestT, ResponseT](http.MethodPost, h)
 }
 
 // Put is a wrapper around a generic handler, forcing the PUT HTTP verb
 func Put[RequestT any, ResponseT response.Response](h handler.GenHandler[RequestT, ResponseT]) http.Handler {
-	return routeIt(http.MethodPut, h)
+	return RouteIt[RequestT, ResponseT](http.MethodPut, h)
 }
 
 // Patch is a wrapper around a generic handler, forcing the PATCH HTTP verb
 func Patch[RequestT any, ResponseT response.Response](h handler.GenHandler[RequestT, ResponseT]) http.Handler {
-	return routeIt(http.MethodPatch, h)
+	return RouteIt[RequestT, ResponseT](http.MethodPatch, h)
 }
 
 // Delete is a wrapper around a generic handler, forcing the DELETE HTTP verb
 func Delete[RequestT any, ResponseT response.Response](h handler.GenHandler[RequestT, ResponseT]) http.Handler {
-	return routeIt(http.MethodDelete, h)
+	return RouteIt[RequestT, ResponseT](http.MethodDelete, h)
 }
