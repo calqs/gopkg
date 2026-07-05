c66458c8976065da993586777b7b96884e85c2cf
monkeydioude <duris.julien@gmail.com>
2026-07-05 19:03:13 +0200

    feat: Select() default to wildcard

--- sqlb/select.go
+++ sqlb/select.go
@@ -1,6 +1,9 @@
 package sqlb
 
 func Select(columns ...string) *Builder {
+	if len(columns) == 0 {
+		columns = []string{"*"}
+	}
 	return &Builder{
 		columns: columns,
 	}
--- sqlb/select_test.go
+++ sqlb/select_test.go
@@ -29,3 +29,11 @@ func TestFromWhereSelect(t *testing.T) {
 	assert.Equal(t, "SELECT name FROM users WHERE id = $1", query)
 	assert.Equal(t, []any{1}, values)
 }
+
+func TestSelectAll(t *testing.T) {
+	wb := Select().From("users").Where(Eq("id", 1))
+	query, values, err := wb.BuildSQL()
+	assert.Nil(t, err)
+	assert.Equal(t, "SELECT * FROM users WHERE id = $1", query)
+	assert.Equal(t, []any{1}, values)
+}
